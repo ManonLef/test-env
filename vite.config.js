f6df3a738b3e3a34cc6494cebcae4372d8b1965d
Manon <manonlefkos@gmail.com>
2023-09-16 21:30:27 +0200

    add test setup file to Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true, // all imports global so no need to perform these imports in each file manually
     environment: "jsdom",
+    setupFiles: "./tests/setup.js",
   },
 });
